7a0c9bb6e19090879225359e27db6d8fe77045f3
Francisco Javier Fernandez Cruz <francisco.fernandez@estech.local>
2023-05-29 13:09:07 +0200

    Creacion de dataclass Francis

---
+++ ProyectoPelis/app/src/main/java/com/example/proyectopelis/ui/PelisEnCine.kt
@@ -0,0 +1,25 @@
+package com.example.proyectopelis.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proyectopelis.R
+import com.example.proyectopelis.databinding.FragmentPelisEnCineBinding
+
+class PelisEnCine : Fragment() {
+
+    private lateinit var binding:FragmentPelisEnCineBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentPelisEnCineBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+}
