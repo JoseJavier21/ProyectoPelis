9671960a54c7c6255796298077dcbc646a59a028
Francisco Javier Fernandez Cruz <francisco.fernandez@estech.local>
2023-05-30 17:53:05 +0200

    ultimo commit

--- ProyectoPelis/app/src/main/java/com/example/proyectopelis/MainActivity.kt
+++ ProyectoPelis/app/src/main/java/com/example/proyectopelis/MainActivity.kt
@@ -4,33 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.proyectopelis.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.fragmentContainerView)
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        val navView:BottomNavigationView=binding.bottomNavigationView
+
+        val appBarConfiguration=AppBarConfiguration(
             setOf(
-                R.id.upComing,
+                R.id.pelisEnCine,
+                R.id.pelisPopulares,
                 R.id.topRated,
-                R.id.pelisPopulares
+                R.id.upComing
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigationView.setupWithNavController(navController)
 
         setContentView(binding.root)
+    }
 
-
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController=findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration)||super.onSupportNavigateUp()
     }
 }
