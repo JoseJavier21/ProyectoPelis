6d1dfbdf273c13e3bdb27fe75a7631d769e1617a
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2023-05-30 20:17:14 +0200

    intento de solucion de errores

--- ProyectoPelis/app/src/main/java/com/example/proyectopelis/data/network/ApiService.kt
+++ ProyectoPelis/app/src/main/java/com/example/proyectopelis/data/network/ApiService.kt
@@ -3,11 +3,7 @@ package com.example.proyectopelis.data.network
 
 import com.example.proyectopelis.data.network.TopRated.ResultRated
 import com.example.proyectopelis.data.network.UpComing.ResultComing
-import com.example.proyectopelis.data.network.Detalles.PelisDetalles
-import com.example.proyectopelis.data.network.Imagenes.PelisImagenes
 import com.example.proyectopelis.data.network.Popular.PelisPopulares
-import com.example.proyectopelis.data.network.Videos.PelisVideos
-import com.example.proyectopelis.data.network.NowPlaying.ResultEnCine
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -19,13 +15,13 @@ interface ApiService {
 
     @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
 
-    @GET("movie/now_playing")
-    suspend fun getPelisEnCine(
-        @Query("language") idioma: String,
-        @Query("pages") pagina:Int
-    ):Response<ResultEnCine>
+//    @GET("movie/now_playing")
+//    suspend fun getPelisEnCine(
+//        @Query("language") idioma: String,
+//        @Query("pages") pagina:Int
+//    ):Response<ResultEnCine>
 
-    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
+    //@Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
     @GET("movie/popular")
     suspend fun getPelisPopulares(
         @Query("language")idioma: String,
@@ -46,25 +42,25 @@ interface ApiService {
         @Query("page")pagina:Int
     ): Response<ResultComing>
 
-    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
-    @GET("movie/{movie_id}")
-    suspend fun damePelisDetalles(
-        @Query("language") idioma : String,
-        @Path("movie_id") idpeli: Int
-    ) : Response<PelisDetalles>
+//    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
+//    @GET("movie/{movie_id}")
+//    suspend fun damePelisDetalles(
+//        @Query("language") idioma : String,
+//        @Path("movie_id") idpeli: Int
+//    ) : Response<PelisDetalles>
 
-    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
-    @GET("movie/{movie_id}/images")
-    suspend fun damePelisImagenes(
-        @Query("language") idioma : String,
-        @Path("movie_id") idpeli: Int
-    ) : Response<PelisImagenes>
+//    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
+//    @GET("movie/{movie_id}/images")
+//    suspend fun damePelisImagenes(
+//        @Query("language") idioma : String,
+//        @Path("movie_id") idpeli: Int
+//    ) : Response<PelisImagenes>
 
-    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
-    @GET("movie/{movie_id}/videos")
-    suspend fun damePelisVideos(
-        @Query("language") idioma : String,
-        @Path("movie_id") idpeli: Int
-    ) : Response<PelisVideos>
+//    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
+//    @GET("movie/{movie_id}/videos")
+//    suspend fun damePelisVideos(
+//        @Query("language") idioma : String,
+//        @Path("movie_id") idpeli: Int
+//    ) : Response<PelisVideos>
 
 }
