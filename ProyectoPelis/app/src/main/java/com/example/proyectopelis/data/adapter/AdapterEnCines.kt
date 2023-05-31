f603b5508e746dd5a1e20fb9722116c5fc8ce954
Juan-Valverde-Guzman <juan.valverde@escuelaestech.es>
2023-05-31 14:24:38 +0200

    borrame

--- ProyectoPelis/app/src/main/java/com/example/proyectopelis/data/adapter/AdapterEnCines.kt
+++ ProyectoPelis/app/src/main/java/com/example/proyectopelis/data/adapter/AdapterEnCines.kt
@@ -1,70 +1,4 @@
-package com.example.proyectopelis.data.adapter
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.Filter
-import android.widget.Filterable
-import androidx.recyclerview.widget.RecyclerView
-import com.example.proyectopelis.data.network.Imagenes.PelisImagenes
-import com.example.proyectopelis.data.network.NowPlaying.ResultEnCine
-import com.example.proyectopelis.databinding.CeldafrancisBinding
-import java.util.ArrayList
-
-
-class AdapterEnCines (val listener:OnItemClickListener):
-    RecyclerView.Adapter<AdapterEnCines.Celda2Holder>(), Filterable{
-
-
-    private var listaEnCines=ArrayList<ResultEnCine>()
-    private var listaCopia = ArrayList<ResultEnCine>()
-
-    private val listaEnCines=ArrayList<ResultEnCine?>()
-
-    private var listaImagenes=ArrayList<PelisImagenes>()
-    private var listaEnCines=ArrayList<ResultEnCine?>()
-    private var listaCopia = ArrayList<ResultEnCine?>()
-
-    interface  OnItemClickListener{
-        fun OnItemClick(resultEnCine: ResultEnCine)
-    }
-
-    inner class Celda2Holder(val binding: CeldafrancisBinding):RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Celda2Holder {
-        val inflater=LayoutInflater.from(parent.context)
-        val binding=CeldafrancisBinding.inflate(inflater,parent,false)
-        return Celda2Holder(binding)
-    }
-
-    override fun onBindViewHolder(holder: Celda2Holder, position: Int) {
-        val enCine=listaEnCines?.get(position)
-        val imagen=listaImagenes?.get(position)
-        //Glide.with(requireContext())).load(imagen?.posters?.get(0)?.file_path).into(bin)
-
-        holder.binding.nPeli.text=enCine?.title
-        holder.binding.flanzamiento.text=enCine?.releaseDate
-        holder.binding.mediaVoto.text=enCine?.voteAverage.toString()
-        holder.itemView.setOnClickListener {
-            if (enCine != null) {
-                listener.OnItemClick(enCine)
-            }
-        }
-    }
-
-    override fun getItemCount(): Int {
-       return listaEnCines.size
-    }
-
-    fun actualizaLista2(lista: List<ResultEnCine?>){
-        listaEnCines.clear()
-        listaEnCines.addAll(lista)
-        notifyDataSetChanged()
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> Francis
     override fun getFilter(): Filter {
         return object : Filter(){
             override fun performFiltering(constraint: CharSequence?): FilterResults {
@@ -85,22 +19,7 @@ class AdapterEnCines (val listener:OnItemClickListener):
                 return filterResult
             }
 
-            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
-                listaEnCines = results?.values as ArrayList<ResultEnCine?>
-                notifyDataSetChanged()
-            }
-
-        }
-    }
-}
-<<<<<<< HEAD
-=======
-}
-
->>>>>>> e18ade48dca65f9bf350f6ed011444d44c1d4237
-=======
-
-
+            override fun publishResults(constraint: CharSequence?, results: FilterResu======>>>>>>> e18ade48dca65f9bf350f6ed01144
 
 
 
