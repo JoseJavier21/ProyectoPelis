883a7af18ea1521f209f8f276307122e6e4afc80
Francisco Javier Fernandez Cruz <francisco.fernandez@estech.local>
2023-06-07 17:30:17 +0200

    añado api service juan a Francis

--- ProyectoPelis/app/src/main/java/com/example/proyectopelis/data/network/ApiService.kt
+++ ProyectoPelis/app/src/main/java/com/example/proyectopelis/data/network/ApiService.kt
@@ -51,23 +51,26 @@ interface ApiService {
     @Headers("Content-Type: application/json")
     @GET("movie/{movie_id}")
     suspend fun damePelisDetalles(
+        @Path("movie_id") idpeli: Int,
         @Query("language") idioma : String,
-        @Path("movie_id") idpeli: Int
+        @Query("api_key") key : String
     ) : Response<PelisDetalles>
 
 
-    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
+    @Headers("Content-Type: application/json")
     @GET("movie/{movie_id}/images")
     suspend fun damePelisImagenes(
+        @Path("movie_id") idpeli: Int,
         @Query("language") idioma : String,
-        @Path("movie_id") idpeli: Int
+        @Query("api_key") key : String
     ) : Response<PelisImagenes>
 
-    @Headers("Content-Type: application/json","api-key: 5f7af1e971090ad23a762fcc923ac6ce")
+    @Headers("Content-Type: application/json")
     @GET("movie/{movie_id}/videos")
     suspend fun damePelisVideos(
+        @Path("movie_id") idpeli: Int,
         @Query("language") idioma : String,
-        @Path("movie_id") idpeli: Int
+        @Query("api_key") key : String
     ) : Response<PelisVideos>
 
 
